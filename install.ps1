d0544daff20722b7332a415a00a1b28544c53e15
swoperado <89522443+swoperado@users.noreply.github.com>
2021-09-21 12:27:58 +0200

    Update install.ps1
    another fix

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # emClient
 choco install em-client -y
