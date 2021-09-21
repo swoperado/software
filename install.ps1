9048586af36179a2793a7a9fc271b4e6b0abede1
swoperado <89522443+swoperado@users.noreply.github.com>
2021-09-21 12:07:07 +0200

    Update install.ps1
    fixed flags

--- install.ps1
+++ install.ps1
@@ -2,16 +2,16 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # emClient
-choco install em-client -A
+choco install em-client -y
 
 # OpenOffice
-choco install openoffice -A
+choco install openoffice -y
 
 # Firefox 
-choco install firefox -A
+choco install firefox -y
 
 # Vscodium
-choco install vscodium -A
+choco install vscodium -y
 
 # Chrome 
-choco install chrome-remote-desktop-chrome -A
+choco install chrome-remote-desktop-chrome -y
