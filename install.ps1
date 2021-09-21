d96638f18001ef6300e74a8c2989c1070ad9541e
swoperado <89522443+swoperado@users.noreply.github.com>
2021-09-21 12:31:05 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Chocolatey
-powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # emClient
 choco install em-client -y
@@ -14,4 +14,4 @@ choco install firefox -y
 choco install vscodium -y
 
 # Chrome 
-choco install chrome-remote-desktop-chrome -y
+#choco install chrome-remote-desktop-chrome -y
